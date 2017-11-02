3c20e2dcbe92c9e478434d1452b530c846797d77
Trayana Koleva <32462171+TraianaK@users.noreply.github.com>
2017-11-02 16:19:43 +0200

    Add files via upload

---
+++ Homework_Loops.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Homework_Loops
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter a number X : ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter a number Y : ");
+        int y = int.Parse(Console.ReadLine());
+
+        if (x < y)
+        {
+            for (int i = x; i<=y; i++)
+            Console.WriteLine(i);
+        }
+        else
+        {
+            for (int i = x; i >= y; i--)
+                Console.WriteLine(i);
+        }
+    }
+}
+
---
+++ MaxNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MaxNumber
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var max = int.MinValue;
+
+        for (int i = 1; i < n; i++)
+        {
+            var num = int.Parse(Console.ReadLine());
+            if (num > max)
+            {
+                num = max;
+            }
+        }
+        Console.WriteLine("Max = " + max);
+    }
+
+}
+
